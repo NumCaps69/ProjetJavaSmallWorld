0c44faaf8f855c839808f7d2a0703bd74c2701e3
Kai <k.belalem@outlook.fr>
2025-12-10 13:54:47 +0100

    changement main pour choisir config pour l'user arrivant

--- smallWorld/src/Main.java
+++ smallWorld/src/Main.java
@@ -1,6 +1,7 @@
 import modele.plateau.*;
 import vuecontroleur.VueControleur;
 import modele.jeu.Jeu;
+import java.util.Scanner;
 
 import javax.swing.*;
 
@@ -12,7 +13,35 @@ public class Main {
         Runnable r = new Runnable() {
             @Override
             public void run() {
-                VueControleur vc = new VueControleur(new Jeu(4, true, 8, 20));
+                System.out.println("CONFIG DU JEU : \n");
+                Scanner s = new Scanner(System.in);
+                System.out.println("Veuillez choisir le nombre de joueurs : (par défaut 2) - min: 2, max : 4");
+                int nbj = s.nextInt();
+                if(nbj < 2 || nbj > 4){
+                    nbj = 2;
+                }
+                System.out.println("Voulez-vous activer les obstacles ? O/N");
+                String ii = s.next();
+                boolean activer;
+                if(ii.equalsIgnoreCase("O")){
+                    activer = true;
+                }
+                else{
+                    activer = false;
+                }
+                System.out.println("Nombres d'unités maximum : (par défaut : 8) - min: 2, max: 12");
+                int nbunits = s.nextInt();
+                if(nbunits < 2 || nbunits > 12){
+                    nbunits = 8;
+                }
+                System.out.println("Combien de tours maximum autorisez vous ? (min : 10, max : 50)");
+                int nbtours = s.nextInt();
+                if(nbtours < 10 || nbtours > 50){
+                    nbtours = 10;
+                }
+                s.close();
+                System.out.println("LE JEU SE LANCE ! \n");
+                VueControleur vc = new VueControleur(new Jeu(nbj, activer, nbunits, nbtours));
                 vc.setVisible(true);
             }
         };
