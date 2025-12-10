25362a97853bea350332941ae90006ead4af94bd
Kai <k.belalem@outlook.fr>
2025-12-10 14:31:00 +0100

    licence

--- smallWorld/src/modele/plateau/Evenement.java
+++ smallWorld/src/modele/plateau/Evenement.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2025 BELALEM KAISSE & KITA DJESSY-ALBERTO
+ *
+ * Ce code a été créé dans le cadre du projet LIFAPOO (SmallWorld en JAVA).
+ * Toute copie, modification ou redistribution sans autorisation est interdite.
+ * Tous droits réservés.
+ */
 package modele.plateau;
 
 /**
